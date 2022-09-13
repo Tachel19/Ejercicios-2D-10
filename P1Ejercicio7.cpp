d36043215322e4f33006424d1459e4e0a8465cc8
Tachel19 <112517032+Tachel19@users.noreply.github.com>
2022-09-13 15:12:55 -0600

    Add files via upload

---
+++ P1Ejercicio7.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double nota1, nota2, nota3, promedio;
+	cout << "Ingrese la primera nota: " << endl;
+	cin >> nota1;
+	cout << "Ingrese la segunda nota: " << endl;
+	cin >> nota2;
+	cout << "Ingrese la tercera nota: " << endl;
+	cin >> nota3;
+	promedio = (nota1 + nota2 + nota3)/3;
+	if (promedio >= 7 and promedio <= 10){
+		cout <<"Aprobado";
+	} else if (promedio >= 4 && promedio < 7){
+		cout << "Reprobado";
+	} else {
+		cout <<"Nota no valida";
+	}
+}
